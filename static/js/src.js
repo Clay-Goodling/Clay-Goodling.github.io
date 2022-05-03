3a9e1c11ca805f3c9865458e0fab2e491c444935
Clay Goodling <jcg284@cornell.edu>
2022-05-03 01:30:21 -0400

    zoom into sub screens

--- static/js/src.js
+++ static/js/src.js
@@ -1,10 +1,18 @@
+function expand(elem) {
+  let bbox = elem.node().getBBox();
+  elem.stlye("position", "absolute")
+    .style("left", bbox.x)
+    .style("top", bbox.y);
+  elem.classed("active", true);
+}
+
 function main() {
   let body = d3.select("#body");
 
   let title = body.append("div")
     .attr("class", "title")
     .append("h1")
-    .text("> James Goodling ");
+    .text("> James Goodling");
 
   let content = body.append("div")
     .attr("class", "content");
@@ -13,12 +21,13 @@ function main() {
     .attr("class", "container");
 
   let frame = about.append("div")
-    .attr("class", "frame");
+    .attr("class", "frame")
+    .on("click", function(){d3.select(this).attr("class", "active")});
 
   frame.append("div")
     .attr("class", "title")
     .append("h1")
-      .text("> James Goodling ");
+      .text("> James Goodling 2");
 
   let i_content = frame.append("div")
     .attr("class", "content")
@@ -26,12 +35,13 @@ function main() {
       .attr("class", "container");
 
   let i_frame = i_content.append("div")
-    .attr("class", "frame");
+    .attr("class", "frame")
+    .on("click", function(){d3.select(this).attr("class", "active")});
 
   i_frame.append("div")
     .attr("class", "title")
     .append("h1")
-      .text("> James Goodling ");
+      .text("> James Goodling 3");
 
   i_frame.append("div")
     .attr("class", "content")
