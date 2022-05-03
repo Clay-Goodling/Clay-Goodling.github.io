78485fd65bba17cf8b5448bc848ab04060a203d0
Clay Goodling <jcg284@cornell.edu>
2022-05-03 00:34:34 -0400

    layout and miniframes

--- static/js/src.js
+++ static/js/src.js
@@ -1,3 +1,40 @@
 function main() {
-  document.getElementById("body").innerHTML = "Hello, World";
+  let body = d3.select("#body");
+
+  let title = body.append("div")
+    .attr("class", "title")
+    .append("h1")
+    .text("> James Goodling ");
+
+  let content = body.append("div")
+    .attr("class", "content");
+
+  let about = content.append("div")
+    .attr("class", "container");
+
+  let frame = about.append("div")
+    .attr("class", "frame");
+
+  frame.append("div")
+    .attr("class", "title")
+    .append("h1")
+      .text("> James Goodling ");
+
+  let i_content = frame.append("div")
+    .attr("class", "content")
+    .append("div")
+      .attr("class", "container");
+
+  let i_frame = i_content.append("div")
+    .attr("class", "frame");
+
+  i_frame.append("div")
+    .attr("class", "title")
+    .append("h1")
+      .text("> James Goodling ");
+
+  i_frame.append("div")
+    .attr("class", "content")
+    .append("div")
+      .attr("class", "container");
 }
