5bd3efcfd30f2eb825a6e89e6a199f0dd52494b5
Clay Goodling <jcg284@cornell.edu>
2022-05-03 21:55:45 -0400

    work placeholders

--- static/js/src.js
+++ static/js/src.js
@@ -1,6 +1,6 @@
 class BackButton {
   constructor(text, elem) {
-    this.elem = d3.select(document.createElement('div'))
+    this.elem = d3.select(document.createElement("div"))
       .classed("button", true)
       .text(text)
       .on("click", function (e) {
@@ -13,7 +13,7 @@ class BackButton {
 
 class TextBox {
   constructor(path) {
-    this.elem = d3.select(document.createElement('div'))
+    this.elem = d3.select(document.createElement("div"))
       .classed("textbox", true);
 
     fetch(path)
@@ -24,14 +24,20 @@ class TextBox {
 
 class Image {
   constructor(path) {
-    this.elem = d3.select(document.createElement('img'))
+    this.elem = d3.select(document.createElement("img"))
     .attr("src", path);
+  }
+}
+
+class ContactForm {
+  constructor() {
+    this.elem = this.elem = d3.select(document.createElement("form"));
   }
 }
 
 class Page {
   constructor(title, back=null, active=false) {
-    this.elem = d3.select(document.createElement('div'))
+    this.elem = d3.select(document.createElement("div"))
       .classed("frame", true);
 
     this.elem.classed("active", active)
@@ -78,28 +84,66 @@ class Page {
 function main() {
   let body = d3.select("#body");
 
-  let main = new Page("James Goodling", null, true);
-  body.node().appendChild(main.elem.node());
+  let home = new Page("James Goodling", null, true);
+  body.node().appendChild(home.elem.node());
 
   let about = new Page("About Me", back="Home");
-  main.addSubPage(about);
+  home.addSubPage(about);
 
-  about.addImage("static/img/James_Goodling_2022.jpg")
-  about.addText('static/text/bio.txt');
+  about.addImage("static/img/James_Goodling_2022.jpg");
+  about.addText("static/text/placeholder.txt");
 
 
   let projects = new Page("Projects", back="Home");
-  main.addSubPage(projects);
+  home.addSubPage(projects);
 
   let personalProjects = new Page("Personal Projects", back="Projects");
   projects.addSubPage(personalProjects);
 
+  let pp1 = new Page("Personal Project 1", "Personal Projects");
+  personalProjects.addSubPage(pp1);
+
+  pp1.addImage("static/img/placeholder.jpg");
+  pp1.addText("static/text/placeholder.txt");
+
+  let pp2 = new Page("Personal Project 2", "Personal Projects");
+  personalProjects.addSubPage(pp2);
+
+  pp2.addText("static/text/placeholder.txt");
+  pp2.addImage("static/img/placeholder.jpg");
+
+  let pp3 = new Page("Personal Project 1", "Personal Projects");
+  personalProjects.addSubPage(pp3);
+
+  pp3.addImage("static/img/placeholder.jpg");
+  pp3.addText("static/text/placeholder.txt");
+
+
   let academicWork = new Page("Academic Work", back="Projects");
   projects.addSubPage(academicWork);
 
+  let aw1 = new Page("Academic Work 1", "Academic Work");
+  academicWork.addSubPage(aw1);
+
+  aw1.addImage("static/img/placeholder.jpg");
+  aw1.addText("static/text/placeholder.txt");
+
+  let aw2 = new Page("Academic Work 2", "Academic Work");
+  academicWork.addSubPage(aw2);
+
+  aw2.addText("static/text/placeholder.txt");
+  aw2.addImage("static/img/placeholder.jpg");
+
+  let aw3 = new Page("Academic Work 1", "Academic Work");
+  academicWork.addSubPage(aw3);
+
+  aw3.addImage("static/img/placeholder.jpg");
+  aw3.addText("static/text/placeholder.txt");
+
 
   let contact = new Page("Contact Me", back="Home");
-  main.addSubPage(contact);
+  home.addSubPage(contact);
 
-  
+  let contactForm = new ContactForm();
+  contact.addForm(contactForm);
 }
