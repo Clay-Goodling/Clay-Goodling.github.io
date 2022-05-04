30a271a0fa79f5fa2f9e1405fd304f8e9dffd802
Clay Goodling <jcg284@cornell.edu>
2022-05-03 21:10:38 -0400

    about setup

--- static/js/src.js
+++ static/js/src.js
@@ -4,9 +4,28 @@ class BackButton {
       .classed("button", true)
       .text(text)
       .on("click", function (e) {
-        elem.classed("active", false);
+        elem.classed("active", false)
+          .classed("mini", true);
         e.stopPropagation();
       })
+  }
+}
+
+class TextBox {
+  constructor(path) {
+    this.elem = d3.select(document.createElement('div'))
+      .classed("textbox", true);
+
+    fetch(path)
+      .then(r => r.text())
+      .then(d => this.elem.append("p").html(d));
+  }
+}
+
+class Image {
+  constructor(path) {
+    this.elem = d3.select(document.createElement('img'))
+    .attr("src", path);
   }
 }
 
@@ -15,13 +34,16 @@ class Page {
     this.elem = d3.select(document.createElement('div'))
       .classed("frame", true);
 
-    if (active) {
-      this.elem.classed("active", true);
-    }
+    this.elem.classed("active", active)
+      .classed("mini", !active);
 
     this.head = this.elem.append("div")
       .attr("class", "title")
-      .on("click", () => this.elem.classed("active", true));
+      .on("click", () =>
+        (this.parent != null && this.parent.classed("active"))
+        ? this.elem.classed("active", true).classed("mini", false)
+        : null
+      );
 
     if (back != null) {
       let back_button = new BackButton(back, this.elem);
@@ -37,9 +59,19 @@ class Page {
 
   addSubPage(page) {
     let container = this.content.append("div")
-      .classed("container", true);
+      .classed("frame-container", true);
 
     container.node().appendChild(page.elem.node());
+
+    page.parent = this.elem;
+  }
+
+  addText(path) {
+    this.content.node().appendChild(new TextBox(path).elem.node());
+  }
+
+  addImage(path) {
+    this.content.node().appendChild(new Image(path).elem.node());
   }
 }
 
@@ -52,52 +84,18 @@ function main() {
   let about = new Page("About Me", back="Home");
   main.addSubPage(about);
 
+  about.addImage("static/img/James_Goodling_2022.jpg")
+  about.addText('static/text/bio.txt');
+
   let projects = new Page("Projects", back="Home");
   main.addSubPage(projects);
+
+  let personalProjects = new Page("Personal Projects", back="Projects");
+  projects.addSubPage(personalProjects);
+
+  let academicWork = new Page("Academic Work", back="Projects");
+  projects.addSubPage(academicWork);
 
   let contact = new Page("Contact Me", back="Home");
   main.addSubPage(contact);
 }
-
-// function main() {
-//   let body = d3.select("#body");
-
-//   let title = body.append("div")
-//     .attr("class", "title")
-//     .append("h1")
-//     .text("> James Goodling");
-
-//   let content = body.append("div")
-//     .attr("class", "content");
-
-//   let about = content.append("div")
-//     .attr("class", "container");
-
-//   let frame = about.append("div")
-//     .attr("class", "frame")
-//     .on("click", function(){d3.select(this).attr("class", "active")});
-
-//   frame.append("div")
-//     .attr("class", "title")
-//     .append("h1")
-//       .text("> James Goodling 2");
-
-//   let i_content = frame.append("div")
-//     .attr("class", "content")
-//     .append("div")
-//       .attr("class", "container");
-
-//   let i_frame = i_content.append("div")
-//     .attr("class", "frame")
-//     .on("click", function(){d3.select(this).attr("class", "active")});
-
-//   i_frame.append("div")
-//     .attr("class", "title")
-//     .append("h1")
-//       .text("> James Goodling 3");
-
-//   i_frame.append("div")
-//     .attr("class", "content")
-//     .append("div")
-//       .attr("class", "container");
-// }
